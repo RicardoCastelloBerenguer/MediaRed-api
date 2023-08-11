86189be239dca1c2c98e355f9f3369151cf133e9
Ricardo <ricardocastellob@gmail.com>
2023-08-11 14:05:05 +0200

    Global Controller hecho (pendiente de revisión)

--- app/services/FileService.php
+++ app/services/FileService.php
@@ -2,13 +2,16 @@
 
 namespace App\Services;
 
+use App\Models\Post;
+use App\Models\User;
 use Image;
+use Request;
 
 class FileService
 {
-    public function updateImage($model, $request)
+    public function updateImage(User $model, $request)
     {
-        $image = Image::make($request->file('image'));
+        $image = \Intervention\Image\Facades\Image::make($request->file('image'));
 
         if (!empty($model->image)) {
 
@@ -22,6 +25,17 @@ class FileService
         $image->crop($request->width, $request->height, $request->left, $request->top);
         $name = time() . '.' . $extension;
         $image->save(public_path() . '/files' . $name);
+
+        return $model;
+    }
+
+    public function addVideo(Post $model, \Illuminate\Http\Request $request)
+    {
+        $video = $request->file('video');
+        $extension = $video->getClientOriginalExtension();
+        $name = time() . '.' . $extension;
+        $video->move(public_path() . '/files', $name);
+        $model->video = '/files/' . $name;
 
         return $model;
     }
