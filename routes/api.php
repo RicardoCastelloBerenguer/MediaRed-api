89de1179f0a264dcdd094c80e07bd6bd97949b68
Ricardo <ricardocastellob@gmail.com>
2023-08-26 09:33:07 +0200

    Relacion de modelos y creación de endpoints

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,12 @@
 <?php
 
+use App\Http\Controllers\Api\CommentController;
+use App\Http\Controllers\Api\GlobalController;
+use App\Http\Controllers\Api\HomeController;
+use App\Http\Controllers\Api\LikeController;
+use App\Http\Controllers\Api\PostController;
 use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -15,6 +21,35 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/get-random-users', [GlobalController::class, 'getRandomUsers']);
+Route::get('/home', [HomeController::class, 'index']);
+Route::get('/get-random-users', [GlobalController::class, 'getRandomUsers']);
+
 Route::middleware(['auth:sanctum'])->group(function () {
+
+    //USER ENDPOINTS
+
     Route::get('/logged-user', [UserController::class, 'loggedUser']);
+    Route::post('/update-user-image', [UserController::class, 'updateUserImage']);
+    Route::patch('/update-user', [UserController::class, 'updateUser']);
+
+    //POSTS ENDPOINTS
+
+    Route::get('/posts/{id}', [PostController::class, 'show']);
+    Route::post('/posts', [PostController::class, 'store']);
+    Route::patch('/posts/{id}', [PostController::class, 'destroy']);
+
+    //PROFILE ENDPOINTS
+
+    Route::get('/profile/{id}', [ProfileController::class, 'show']);
+
+    //COMMENTS ENDPOINTS
+
+    Route::post('/comments', [CommentController::class, 'store']);
+    Route::delete('/comments/{id}', [CommentController::class, 'destroy']);
+
+    //LIKES ENDPOINTS
+
+    Route::post('/likes' . [LikeController::class, 'store']);
+    Route::post('/likes/{id}' . [LikeController::class, 'show']);
 });
